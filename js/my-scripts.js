8a72d32449d2f41122a8531b272ae07fd5a34add
Sam MBP <snevarez89@gmail.com>
2023-09-13 02:15:52 -0700

    new styles in september 2023

---
+++ js/my-scripts.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here that manipulates the <head> section
+    // This code will execute after the <head> is fully loaded, 
+    // but before the <body> is loaded.
+});
+
+const htmlContent = `
+<!-- fonts start-->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Victor+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+<!-- fonts end-->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TK7VWZ9VVG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TK7VWZ9VVG');
+</script>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Custom styles for this template -->
+
+<script src="/js/jquery.js"></script>
+<script src="/js/lity.js"></script>
+
+
+<link rel="stylesheet" href="/css/01-base.all.css">
+<link rel="stylesheet" href="/css/01-base/bootstrap-css.css">
+<link rel="stylesheet" href="/css/01-base/style.css">
+<link rel="stylesheet" href="/css/02-components/component-style.css">
+
+<!-- favicon -->
+<link rel="icon" type="image/svg+xml" href="img/favicon/favicon.svg">
+<link rel="icon" type="image/png" href="img/favicon/favicon.png">
+
+<!-- Bootstrap core JavaScript -->
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Jquery easing -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+`;
